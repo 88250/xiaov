0d19ebedbf70074b2f7d5b87fba7f94e40d4c05e
Liang Ding <DL88250@gmail.com>
2017-10-31 21:03:13 +0800

    :bug: 避免自聊死循环

--- src/main/java/org/b3log/xiaov/service/QQService.java
+++ src/main/java/org/b3log/xiaov/service/QQService.java
@@ -141,17 +141,17 @@ public class QQService {
     /**
      * XiaoV self intro. Built-in advertisement.
      */
-    private static final String XIAO_V_INTRO = "你好，我是小薇~ 关于我的更多资料请看帖 https://hacpai.com/article/1467011936362";
+    private static final String XIAO_V_INTRO = "关于我的更多资料请看帖 https://hacpai.com/article/1467011936362";
 
     /**
      * XiaoV listener self intro.
      */
-    private static final String XIAO_V_LISTENER_INTRO = "你好，我是小薇的守护，关于我的更多资料请看帖 https://hacpai.com/article/1467011936362";
+    private static final String XIAO_V_LISTENER_INTRO = "关于我的更多资料请看帖 https://hacpai.com/article/1467011936362";
 
     /**
      * No listener message.
      */
-    private static final String NO_LISTENER = "请把我的守护也拉进群，否则会造成大量消息重复（如果已经拉了，那就稍等 10 秒钟，我的守护可能在醒瞌睡 O(∩_∩)O哈哈~）\n\nPS：小薇机器人使用问题请看帖 https://hacpai.com/article/1467011936362";
+    private static final String NO_LISTENER = "请把我的守护也拉进群，否则会造成大量消息重复（如果已经拉了，那就稍等 10 秒钟，我的守护可能在醒瞌睡 O(∩_∩)O哈哈~）\n\nPS：机器人使用问题请看帖 https://hacpai.com/article/1467011936362";
 
     /**
      * 超过 {@value #PUSH_GROUP_USER_COUNT} 个成员的群才推送.
